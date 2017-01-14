ee662c6e8a7985e1f60b4b1896b87981ae60c22f
Stuart <stuwashere@gmail.com>
2017-01-13 18:39:20 -0800

    improving assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require bootstrap/modal
+//= require bootstrap/dropdown
+//= require bootstrap/alert
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/jquery.dataTables
